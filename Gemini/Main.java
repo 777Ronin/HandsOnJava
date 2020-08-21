2f5b9d8ac594c1b522f19cf7e70479d4f07b6fc4
Daniel De Lima <dcfdelima@gmail.com>
2020-08-21 14:54:40 -0700

    initial commit

---
+++ Gemini/Main.java
@@ -0,0 +1,16 @@
+package com.Gemini;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        int i = 0;
+
+        while(i<5){
+            System.out.println("i: " + i);
+            i++;
+        }
+
+    }
+}
